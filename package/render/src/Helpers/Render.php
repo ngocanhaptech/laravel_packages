d5ec5597466aea1218ead06a16ace1e95835cfb3
ngocanhaptech <ngocanhaptech@gmail.com>
2020-03-23 02:31:06 +0700

    remove render dd

--- package/render/src/Helpers/Render.php
+++ package/render/src/Helpers/Render.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Storage;
 class Render {
     public static function index($viewName) {
         $content = view($viewName)->render();
-        $filename = str_replace(['::', ' '], '_', $viewName); 
-        dd($viewName, $filename);
+        $filename = str_replace(['::', ' '], '_', $viewName);
         Storage::disk('public')->put(config('render.dist') . preg_replace('/[^A-Za-z0-9\-]/', '-', $filename) . '.html' , $content);
         return view($viewName);
     }
